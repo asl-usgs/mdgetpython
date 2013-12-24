c39a1cd4162324fecbe360c06663adbdab76ef5c
aringler-usgs <aringler@usgs.gov>
2013-12-24 11:12:15 -0700

    I added resp parsing.  I also deal with multiple epochs.  We need to figure out the timing issue on the socket

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ in a very crude form
 
 To DO
 ===========
-Need to setup command line parsing
-Need to allow for multiple stations
-Need to identify the correct object format to send back
-Need to include a get pole/zero function
-Need to setup a main and all the fancy python stuff
 
+Need to setup command line parsing
+Need to verify wild cards for odd situations
+Need to setup a main and all the fancy python stuff
+Need to verify various epochs
+Need to figure out the timing callbacks since the code is timing out on our end
+Need to include error handling
--- mdget.py
+++ mdget.py
@@ -17,16 +17,16 @@ from obspy.core import UTCDateTime
 #######################################################################
 
 #Variables that will not change often
-debug = False
+debug = True
 host = "137.227.224.97"
 port = 2052
-maxslept = 30 / 0.05
-maxblock = 10240
+maxslept = 60 / 0.05
+maxblock = 2*10240
 
 #Here are the variables needed to be parsed
-station = "MACI"
-location =""
-channel ="BHZ"
+station = "BBSR"
+location ="*"
+channel ="LHZ"
 network ="IU"
 
 stime = UTCDateTime("2012-01-01T00:00:00.0")
@@ -34,11 +34,22 @@ stime = UTCDateTime("2012-01-01T00:00:00.0")
 def getString(net,sta,loc,chan):
 #This function formats the string to be used in mdget
 	debugmdgetString = False
-
+#No wild carding of networks
 	mdgetString = "-s " + net
-	mdgetString += sta.ljust(5,'-')
-	mdgetString += chan
-	mdgetString += loc.ljust(2,'-')
+
+#Now we split with * and append a . in front to allow for wild cards
+	if "*" in sta:
+		mdgetString += ".*".join(sta.split("*"))		
+	else:
+		mdgetString += sta.ljust(5,'-')
+	if "*" in chan:
+		mdgetString += ".*".join(chan.split("*"))
+	else:
+		mdgetString += chan
+	if loc == "*":
+		mdgetString += ".*".join(loc.split("*"))
+	else:	
+		mdgetString += loc.ljust(2,'-')
 	if debugmdgetString:
 		print 'Here is the string: ' + mdgetString
 	return mdgetString
@@ -67,19 +78,69 @@ def parseresp(data):
 	debugParseresp = False
 #Here we parse the 
 	datalessobject = {}
-	data = data.split("\n")
 	if debugParseresp:
 		print data[0]
-#Get the network
-	
+#Get the network as well as the other parameters that are easy
 	datalessobject['Network'] = getvalue("* NETWORK",data)
-	datalessobject['Description'] = getvalue("* DESCRIPTION",data)
 	datalessobject['Station'] = getvalue("* STATION",data)
-	datalessobject['Component'] = getvalue("* COMPONENT",data)
+	datalessobject['Channel'] = getvalue("* COMPONENT",data)
 	datalessobject['Location'] = getvalue("* LOCATION",data)
-	datalessobject['Sensitivity'] = getvalue("* SENS-SEED",data)
+	datalessobject['Start Date'] = getvalue("* EFFECTIVE",data)
+	datalessobject['End Date'] = getvalue("* ENDDATE",data)
+	datalessobject['Input Unit'] = getvalue("* INPUT UNIT",data)
+	datalessobject['Output Unit'] = getvalue("* OUTPUT UNIT",data)
+	datalessobject['Description'] = getvalue("* DESCRIPTION",data)
+	datalessobject['Sampling Rate'] = getvalue("* RATE (HZ)",data)
 	datalessobject['Latitude'] = getvalue("* LAT-SEED",data)
 	datalessobject['Longitude'] = getvalue("* LONG-SEED",data)
+	datalessobject['Elevation'] = getvalue("* ELEV-SEED",data)
+	datalessobject['Depth'] = getvalue("* DEPTH",data)
+	datalessobject['Dip'] = getvalue("* DIP",data)
+	datalessobject['Azimuth'] = getvalue("* AZIMUTH",data)	
+	datalessobject['Instrument Type'] = getvalue("* INSTRMNTTYPE",data)
+	datalessobject['Sensitivity'] = getvalue("* SENS-SEED",data)
+	
+#Now we need to get the poles and zeros
+	nzeros = int(getvalue("ZEROS",data))
+	npoles = int(getvalue("POLES",data))
+	zerosindex = data.index("* ****") + 3
+	polesindex = zerosindex + nzeros + 1
+	poles = []
+	zeros = []
+	if debugParseresp:
+		print 'Here is the index of the zeros: ' + str(zerosindex)
+		print 'Here is the index of the poles: ' + str(polesindex)
+		print 'Here is the first zero: ' + str(data[zerosindex])
+		print 'Here is the first pole: ' + str(data[polesindex])
+		print 'Here is the number of zeros: ' + str(nzeros)
+		print 'Here is the number of poles: ' + str(npoles)
+
+#Lets go through the zeros
+	for zstart in range(0,nzeros):
+#Remove all of the extra white spaces
+		curzero = ' '.join(data[zerosindex + zstart].split())
+		if debugParseresp:
+			print 'Here is what is coming in: ' + curzero
+#Split into two		
+		curzero = curzero.split()
+		curzero = float(curzero[0]) + 1j*float(curzero[1])
+		zeros.append(curzero)
+		if debugParseresp:
+			print 'Here is another zero:' + str(curzero)
+	datalessobject['Zeros'] = zeros
+#Lets start dealing with the poles
+	for pstart in range(0,npoles):
+		curpole = ' '.join(data[polesindex + pstart].split())
+		if debugParseresp:
+			print 'Here is what is coming in: ' + curpole
+#Split into two		
+		curpole = curpole.split()
+		curpole = float(curpole[0]) + 1j*float(curpole[1])
+		poles.append(curpole)
+		if debugParseresp:
+			print 'Here is another pole:' + str(curpole)
+	datalessobject['Poles'] = poles	
+
 	return  datalessobject
 
 def getvalue(strSearch, data):
@@ -90,6 +151,10 @@ def getvalue(strSearch, data):
 	value = (value[0].replace(strSearch,'')).strip()
 	return value
 
+
+
+
+
 #We need a function to parse the imput string for various epochs
 importstring = getString(network,station,location,channel)
 
@@ -99,28 +164,41 @@ s.connect((host, port))
 
 #Set blocking we will want a different timeout approach
 s.setblocking(maxslept)
+
+
+importstring += ' -b ' + modifyDateTime(stime)
+importstring +=  " -c r\n"
 if debug:
 	print importstring
 
-importstring += ' -b ' + modifyDateTime(stime)
-importstring +=  " -c r \n"
 s.sendall(importstring)
 
 #Now lets get the data
-data = s.recv(maxblock)
+getmoredata = True
+datagood = []
+while getmoredata:
+#Pulling one epoch of data and splitting it
+	data = (s.recv(maxblock)).split("\n")
+	if debug:
+		print data
+		print 'Okay moving on'
+
+#Check if we are at the end of the return or not
+	if data[-2] == '* <EOR>':
+		if debug:
+			print 'Found the end of the output'
+		getmoredata = False
+#Need to parse the response and add it to the datagood variable
+	if 'no channels found' in data[0]:
+		print 'No channels found\n'
+	else:
+		datagood.append(parseresp(data))
+
 s.close()
 
-#Now we have the data lets parse it
-if debug:
-	print data
-
-datalessobject = parseresp(data)
-print datalessobject
-
-
-
-
-
+for datablock in datagood:
+	print 'Parsed data for: ' + datablock['Station'] + ' ' + \
+		datablock['Location'] + ' ' + datablock['Channel']
 
 
 
